5bb03be9f14b0f53a0fb23d6b0310767ea312c96
possum-enjoyer <15869016+possum-enjoyer@users.noreply.github.com>
2022-10-09 18:46:58 +0200

    Added new Home View

---
+++ src/Home.tsx
@@ -0,0 +1,21 @@
+import React, { type PropsWithChildren } from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+import { AnimatedFAB  } from 'react-native-paper';
+
+const Home = () => {
+    const isDarkMode = useColorScheme() === "dark";
+
+    return <SafeAreaView>
+
+    </SafeAreaView>;
+};
+
+export default Home;
