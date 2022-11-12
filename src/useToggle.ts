536232979b62faeab6a439be91e686d4bd168593
possum-enjoyer <15869016+possum-enjoyer@users.noreply.github.com>
2022-11-13 00:32:14 +0100

    removed useToggle Hook

--- src/useToggle.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export const useToggle = (initialValue: boolean): [toggle: boolean, switchToggle: () => void] => {
-    const [toggle, setToggle] = useState<boolean>(initialValue);
-    return [
-        toggle,
-        () => setToggle((oldToggle) => !oldToggle),
-    ];
-};
